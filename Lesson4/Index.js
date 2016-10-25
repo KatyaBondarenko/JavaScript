3bf1ea216dcca07bee20406c3f2f140ed1f038e6
KatyaBondarenko <superKatya25>
2016-10-25 17:13:39 +0300

    table is added

---
+++ Lesson4/Index.js
@@ -0,0 +1,98 @@
+
+
+
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+
+        var list = document.querySelector('.Table_header');
+     //   var fragment = document.createDocumentFragment();
+
+  /*      function getProperties(arr) {
+
+            for (var prop in arr)
+
+            return prop;
+        }
+
+        data[0].forEach(function(item){
+
+            var element = document.createElement("td");
+
+            fragment.appendChild(element).innerText = getProperties(item);
+
+        });
+
+        list.appendChild(fragment);
+*/
+
+        console.log(data);
+
+        var headerTitles = "";
+
+            for (var key in data[0]) {
+
+                headerTitles = headerTitles + "<th>" + key + "</th>";
+
+            }
+
+        var bodyData = "";
+
+        data.forEach(function(item){
+
+            bodyData = bodyData + "<tr>";
+
+            for (var key in item) {
+
+                var node = item[key];
+
+                if (key === "address" || key === "company"){
+
+                    var nodeContent = "<ul>";
+
+                   for (var key2 in item[key]){
+                       
+
+                       node  = node + "<li>" + key2 + " : " + item[key][key2] + "</li>";
+                   }
+
+                }
+
+                bodyData = bodyData + "<td><pre>" + node + "</pre></td>";
+
+            }
+
+            bodyData = bodyData + "</tr>";
+
+
+        });
+
+        var bodyList = document.querySelector('.Table_body');
+
+        list.innerHTML = headerTitles;
+        bodyList.innerHTML = bodyData;
+
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
